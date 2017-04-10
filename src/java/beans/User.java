3df7316278b8f19064ff9a3f5bbb1ec48e02eab2
c0687799 <c0687799@SB-N208-30.lambton.on.ca>
2017-04-10 15:52:22 -0400

    Deleted a few unnecessary files, added in commenting

--- src/java/beans/User.java
+++ src/java/beans/User.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 package beans;
 
@@ -5,7 +10,10 @@ import javax.json.Json;
 import javax.json.JsonObject;
 
 public class User {
-
+    
+    /**
+     * 
+     */
     public int id;
     public String username;
     public String passhash;
